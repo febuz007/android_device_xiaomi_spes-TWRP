dbe9c5b64493bc538276c55ff0e4a2daa62194da
febuz007 <32975534+febuz007@users.noreply.github.com>
2022-12-16 17:23:55 +0700

    Update and rename twrp_spes.mk to twrp_sunny.mk

--- twrp_sunny.mk
+++ twrp_sunny.mk
@@ -11,7 +11,7 @@ $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from spes/spesn device
-$(call inherit-product, device/xiaomi/spes/device.mk)
+$(call inherit-product, device/xiaomi/sunny/device.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
@@ -20,10 +20,10 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 BOARD_VENDOR := Redmi
 PRODUCT_BRAND := Redmi
 
-PRODUCT_DEVICE := spes
-PRODUCT_NAME := twrp_spes
+PRODUCT_DEVICE := sunny
+PRODUCT_NAME := twrp_sunny
 PRODUCT_MANUFACTURER := xiaomi
-PRODUCT_MODEL := Redmi Note 11
-PRODUCT_RELEASE_NAME := spes
+PRODUCT_MODEL := Redmi Note 10
+PRODUCT_RELEASE_NAME := sunny
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
